c826df51e409091399fc5a230a368813160a40ab
Marcos Javier Pacheco Arizaga <65028325+MarcosPacheco19@users.noreply.github.com>
2023-05-30 02:41:11 -0500

    Update settings.py

--- examenPractico/settings.py
+++ examenPractico/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%!fupgfs&bw4^+-y46h=2@tpb$$=!funoozsstimwi5y3(ka8p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
